68fe571edc9e83c15cf7ab8967815a1099ef880e
Glitch (curious-puma) <none>
2018-06-19 16:08:00 +0000

    👓🏏 Checkpoint ./src/components/surprise.js:909318/597

--- src/components/surprise.js
+++ src/components/surprise.js
@@ -6,10 +6,33 @@ import SurpriseImage from './surprise-image';
 export default class Surprise extends React.Component {
     constructor(props) {
         super(props);
+      this.state = {
+          display : 'button',
+      };
+      
     }
+  
+  //function show image
+  showImage(){
+      this.setState({
+        display: 'imageCall',
+      });
+  }
+  
+ //function show btn
+  showButton(){
+      this.setState({
+        display: 'button',
+      });
+  }
 
     render() {
         // Show the button to start with
-        return <SurpriseButton />;
+      if(this.state.display === 'button'){        
+        return <SurpriseButton onClick={e => this.showImage()}/>;
     }
+    else if(this.state.display === 'imageCall'){
+      return <SurpriseImage onClick={e => this.showButton()}/>
+    }
+      
 }
